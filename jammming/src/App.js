3b3be33d263588d6afd918b2d454dfa666563e22
Stephen <knewcid1234@gmail.com>
2023-08-17 15:57:08 -0400

    add css and js for the App

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -1,13 +1,40 @@
+import React from 'react';
 import './App.css';
+import './SearchBar';
 
 function App() {
+
+  const SearchBar = () => {
+    return (
+      <form>
+        <input 
+          htmlFor='search bar'
+          type='search'
+          id='searchBar'
+          name='searchBar'
+        />
+      </form>
+    );
+  };
+  
+const SearchResults = () => {
   return (
-    <div className="App">
+    <ul>
+      <li>song pick 1</li>
+      <li>song pick 2</li>
+      <li>song pick 3</li>
+    </ul>
+  );
+}
+
+  return (
+    <>
       <header className="App-header">
-        
-        
+        <h1>Jamming</h1>
       </header>
-    </div>
+      <SearchBar />
+      <SearchResults />
+    </>
   );
 }
 
