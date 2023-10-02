e97db185431907fa1edf840031ed92e33426725a
Stephen <knewcid1234@gmail.com>
2023-10-02 10:49:30 -0400

    Refactor and seperated components

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -1,76 +1,19 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './App.css';
 import './SearchBar.css';
+import SearchBar from './searchBar.js';
+import SearchResults from './searchResults';
+import PlayList from './playList';
 
 function App() {
+const [song, setSong] = useState(['Sometimes The Hammer, Sometimes The Nail']);
+const [artist, setArtist] = useState(['A Day To Remember']);
+const [albumn, setAlbumn] = useState(['C, C']);
 
+const [customSong, setCustomSongTitle] = useState(['Enter Sandman']);
+const [customArtist, setCustomArtist] = useState('Metalica');
+const [customAlbumn, setCustomAlbumn] = useState('Poop');
   
-
-  const SearchBar = () => {
-    return (
-      <div className='container'>
-        <form className='formContainer'>
-          <input 
-            htmlFor='search bar'
-            type='search'
-            id='searchBar'
-            name='searchBar'
-            placeholder='Search for Songs'
-            className='searchbar'
-          />
-          <br/><button type='submit' className='searchbutton'>Search</button>
-        </form>
-      </div>
-      
-    );
-  };
-  
-const SearchResults = () => {
-  return (
-    <ul className='card'>
-      <li className='songlist'>
-        <div className='songs'>
-          <span className='songtitle'>Song Title 1</span>
-          <br/><span className='songartist'>by: ~Artist Name~, </span>
-          <span className='albumntitle'>Albumn Title</span>
-        </div>
-        <button type='submit' className='addbutton'>+</button>
-      </li>
-      <li className='songlist'>
-        <div className='songs'>
-          <span className='songtitle'>song pick 2 but this text is here to rep. some long name</span>
-          <br /><span className='songartist'>by: ~Artist Name~, </span>
-          <span className='albumntitle'>Albumn Title</span>
-          </div>
-        <button type='submit' className='addbutton'>+</button>
-      </li>
-      <li className='songlist'>
-        <div className='songs'>
-          <span className='songtitle'>song pick some long name</span>
-          <br /><span className='songartist'>by: ~Artist Name~, </span>
-          <span className='albumntitle'>Albumn Title</span>
-          </div>
-        <button type='submit' className='addbutton'>+</button>
-      </li>      
-    </ul>
-  );
-}
-
-const PlayList = () => {
-  return (
-    <ul className='card'>
-      <li className='songlist'>
-        <button type='submit' className='removebutton'>-</button>
-        <div className='songs'>
-          <span className='songtitle'>Song on playlist 1</span>
-          <br/><span className='songartist'>Artitst </span>
-          <span className='albumntitle'>albumn</span>
-        </div>
-        </li>      
-    </ul>
-  );
-}
-
   return (
     <>
       <header className="App-header">
@@ -80,15 +23,13 @@ const PlayList = () => {
       <main className='container'>
         <section>
           <h2>Search Results</h2>
-          <SearchResults />
+          <SearchResults song={song} artist={artist} albumn={albumn}/>
         </section>
         <section>
           <h2>Playlist</h2>
-          <PlayList />
+          <PlayList song= {customSong} artist={customArtist} albumn={customAlbumn}/>
         </section>
-        
       </main>
-      
     </>
   );
 }
